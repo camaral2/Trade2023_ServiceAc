452de942ed338aace40947a9dcf38ba9650ae609
camaral2 <cristian_amaral@hotmail.com>
2023-06-25 19:54:48 -0300

    Setup correct microservice

--- src/acao/acao.controller.ts
+++ src/acao/acao.controller.ts
@@ -3,6 +3,7 @@ import { AcaoService } from './acao.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { acaoTodayDto } from './dto/acaoToday.dto';
 import { AcaoDto } from './dto/acao.dto';
+import logger from '../utils/logger';
 
 @Controller()
 export class AcaoController {
@@ -10,6 +11,7 @@ export class AcaoController {
 
   @MessagePattern('get_acao_today')
   async getAcaoToday(@Payload() payload: acaoTodayDto): Promise<AcaoDto> {
+    logger.log('Get get_acao_today: ' + payload.acao);
     const acaoToday = await this.acaoService.getAcaoToday(payload.acao);
     return acaoToday;
   }
