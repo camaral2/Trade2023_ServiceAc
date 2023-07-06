be23e1055f6ee87f40ba7ee6efb4158275efdba7
camaral2 <cristian_amaral@hotmail.com>
2023-07-06 20:01:27 -0300

    Complement dependent of test

--- src/log-processo/log-processo.controller.spec.ts
+++ src/log-processo/log-processo.controller.spec.ts
@@ -1,15 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LogProcessoController } from './log-processo.controller';
+import { LogProcessoService } from './log-processo.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { processo } from './entities/processo.entity';
+import { configAcao } from './../acao/entities/configAcao.entity';
 
 describe('LogProcessoController', () => {
   let controller: LogProcessoController;
+  //let service: LogProcessoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [LogProcessoController],
+      providers: [
+        LogProcessoService,
+        {
+          provide: getRepositoryToken(configAcao),
+          useValue: {
+            update: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+        {
+          provide: getRepositoryToken(processo),
+          useValue: {
+            update: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<LogProcessoController>(LogProcessoController);
+    //service = module.get<LogProcessoService>(LogProcessoService);
   });
 
   it('should be defined', () => {
