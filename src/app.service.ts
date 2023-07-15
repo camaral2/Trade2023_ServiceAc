40408489e39353270f576dea2b86f3792a3eba1b
camaral2 <cristian_amaral@hotmail.com>
2023-07-15 18:17:06 -0300

    Identification of service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getVersion(): any {
     return {
-      app: process.env.npm_package_name,
+      app: 'Trade2023_ServiceAc',
       version: process.env.npm_package_version,
       author: 'Cristian dos Santos Amaral',
       email: 'cristian_amaral@hotmail.com',
